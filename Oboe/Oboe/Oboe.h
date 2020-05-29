3c894fd3e58aa882b1aeee086d1405bff3ec0f5a
lichangwen <lichangwen@yy.com>
2020-05-29 16:09:13 +0800

    create framework project

---
+++ Oboe/Oboe/Oboe.h
@@ -0,0 +1,19 @@
+//
+//  Oboe.h
+//  Oboe
+//
+//  Created by lianleven on 2020/5/29.
+//  Copyright © 2020 lianleven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Oboe.
+FOUNDATION_EXPORT double OboeVersionNumber;
+
+//! Project version string for Oboe.
+FOUNDATION_EXPORT const unsigned char OboeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Oboe/PublicHeader.h>
+
+
